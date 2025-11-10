c514a46212d6193c2252ff8ea5098a15e61867c9
Shaik Anisul Rahaman <shairahaman.csc@gmail.com>
2025-11-10 18:44:57 +0000

    Try fixing this error: `Runtime Error: Error: useSidebar is not defined.

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -14,6 +14,7 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
   SidebarTrigger,
+  useSidebar,
 } from '@/components/ui/sidebar';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Separator } from '@/components/ui/separator';
