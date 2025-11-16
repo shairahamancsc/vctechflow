f4e79ff4eddf9d53eedfc031307d036e5192851e
Shaik Anisul Rahaman <shairahaman.csc@gmail.com>
2025-11-16 14:58:24 +0000

    fix this 20:26:29.260 Running build in Washington, D.C., USA (East) – ia

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,20 +1,20 @@
 
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.SERVER_FIREBASE_PROJECT_ID ?? process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.SERVER_FIREBASE_APP_ID ?? process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.SERVER_FIREBASE_API_KEY ?? process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.SERVER_FIREBASE_AUTH_DOMAIN ?? process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.SERVER_FIREBASE_MEASUREMENT_ID ?? process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  storageBucket: process.env.SERVER_FIREBASE_STORAGE_BUCKET ?? process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.SERVER_FIREBASE_MESSAGING_SENDER_ID ?? process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 export const serverFirebaseConfig = {
-    projectId: process.env.SERVER_FIREBASE_PROJECT_ID,
-    appId: process.env.SERVER_FIREBASE_APP_ID,
-    apiKey: process.env.SERVER_FIREBASE_API_KEY,
-    authDomain: process.env.SERVER_FIREBASE_AUTH_DOMAIN,
-    measurementId: process.env.SERVER_FIREBASE_MEASUREMENT_ID,
-    storageBucket: process.env.SERVER_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.SERVER_FIREBASE_MESSAGING_SENDER_ID,
+    projectId: process.env.SERVER_FIREBASE_PROJECT_ID ?? process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    appId: process.env.SERVER_FIREBASE_APP_ID ?? process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: process.env.SERVER_FIREBASE_API_KEY ?? process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.SERVER_FIREBASE_AUTH_DOMAIN ?? process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    measurementId: process.env.SERVER_FIREBASE_MEASUREMENT_ID ?? process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    storageBucket: process.env.SERVER_FIREBASE_STORAGE_BUCKET ?? process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.SERVER_FIREBASE_MESSAGING_SENDER_ID ?? process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
