d6b06067e6be03223988bd05b6518e4118ed3c50
Shaik Anisul Rahaman <shairahaman.csc@gmail.com>
2025-11-16 15:01:18 +0000

    not fix  error 20:29:06.415 Running build in Washington, D.C., USA (East

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -1,10 +1,21 @@
 
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { serverFirebaseConfig } from './config';
+
+const serverFirebaseConfig = {
+    projectId: process.env.SERVER_FIREBASE_PROJECT_ID,
+    appId: process.env.SERVER_FIREBASE_APP_ID,
+    apiKey: process.env.SERVER_FIREBASE_API_KEY,
+    authDomain: process.env.SERVER_FIREBASE_AUTH_DOMAIN,
+    measurementId: process.env.SERVER_FIREBASE_MEASUREMENT_ID,
+    storageBucket: process.env.SERVER_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.SERVER_FIREBASE_MESSAGING_SENDER_ID,
+};
+
 
 let app: FirebaseApp;
-if (!getApps().length) {
+// This guard is needed to prevent re-initializing the app on hot reloads.
+if (!getApps().find(app => app.name === 'server')) {
     app = initializeApp(serverFirebaseConfig, 'server');
 } else {
     app = getApp('server');
