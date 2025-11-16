9c2998d3bb0d95030699598b0560581ca34dcddc
Shaik Anisul Rahaman <shairahaman.csc@gmail.com>
2025-11-16 14:55:50 +0000

    make compatible for vercel

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -1,24 +1,15 @@
 
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { serverFirebaseConfig } from './config';
 
-// Initialize Firebase for server-side usage
+let app: FirebaseApp;
 if (!getApps().length) {
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to server firebase config object.', e);
-        initializeApp(serverFirebaseConfig);
-      } else {
-        initializeApp(serverFirebaseConfig);
-      }
-    }
+    app = initializeApp(serverFirebaseConfig, 'server');
+} else {
+    app = getApp('server');
 }
 
-const app = getApp();
 const db = getFirestore(app);
 
 export { db, app };
